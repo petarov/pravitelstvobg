645df2135ab27759f56fee919928fa153d88b25b
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-05-03 13:04:13 +0200

    Basic RSS data now displayed in a list view.

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -53,12 +53,37 @@ var app = {
      * Get all latest news entries for given section
      */
     onRefresh: function() {
-	
+
     	grss.fetch(grss.URL_EVENTS, function(results, error) {
+//	    	var $page = $("div:jqmData(role='page')");
+	    	var $page = $("div[id='page-n']");
+	    	var $content = $page.children( ":jqmData(role=content)" );
+	    	
     		if (error) {
-    			console.log('all ok');
+    			$content.html('<p>Error loading!</p>');
     		} else {
+    			if (results.length > 0) {
+    				
+    				var tpl = '<ul data-role="listview" data-inset="true" data-filter="false"><li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="arrow-r" data-iconpos="right" data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-c"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><a href="$$LINK$$" class="ui-link-inherit"><h3 class="ui-li-heading">$$TITLE$$</h3><p class="ui-li-desc">$$DESC$$</p></a></div><span class="ui-icon ui-icon-arrow-r ui-icon-shadow">&nbsp;</span></div></li></ul>';
+    				var mark = '';
+    				
+    				for (var i = 0; i < results.length; i++) {
+    					var entry = tpl;
+    					entry = entry.replace('$$TITLE$$', results[i].title);
+    					entry = entry.replace('$$DESC$$', results[i].title);
+    					entry = entry.replace('$$LINK$$', results[i].link);
+    					mark += entry;
+    				}
+    				// inject
+    		    	$content.html(mark);
+    		    	// enhance
+    		    	$page.page();
+    		    	$content.find( ":jqmData(role=listview)" ).listview();
     			
+    			} else {
+    				// TODO
+    				alert('No new items!');
+    			}
     		}
     	});
 	
@@ -68,18 +93,12 @@ var app = {
 	 * Update DOM on a Received Event
 	 */
     receivedEvent: function(id) {
-    
-		/* keep footer visible at all times 
-		 * $(':jqmData(role=footer)').fixedtoolbar( { tapToggle: false } );
-		 */
-    
-//        var parentElement = document.getElementById(id);
-//        var listeningElement = parentElement.querySelector('.listening');
-//        var receivedElement = parentElement.querySelector('.received');
-//
-//        listeningElement.setAttribute('style', 'display:none;');
-//        receivedElement.setAttribute('style', 'display:block;');
+    	console.log('Received Event: ' + id);
     	
-        console.log('Received Event: ' + id);
+    	if (id == 'deviceready') {
+    		// keep footer visible at all times
+//    		$(':jqmData(role=header)').fixedtoolbar( { tapToggle: false } );
+//    		$(':jqmData(role=footer)').fixedtoolbar( { tapToggle: false } );
+    	}
     }
 };
