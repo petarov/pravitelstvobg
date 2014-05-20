361f34dee114b664cd685aee88f760deca9a2cc9
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-05-20 22:57:21 +0200

    Added hook that replaces the android manifest file

---
+++ hooks/after_build/android_manifest.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+// this plugin replaces arbitrary text in arbitrary files
+//
+// Look for the string CONFIGURE HERE for areas that need configuration
+//
+
+var fs = require('fs')
+  , path = require('path')
+  , rootdir = process.argv[2];
+
+
+function quit(msg) {
+    console.error('[ERROR] ', msg);
+    process.exit(-1);
+};
+
+var manifestPath = path.join(rootdir, 'platforms/AndroidManifest.xml');
+if (!fs.existsSync(manifestPath)) {
+    quit(manifestPath + ' does not exist!');
+}
+
+var dest = path.join(rootdir, 'platforms/android');
+if (!fs.existsSync(dest)) {
+    quit(manifestPath + ' does not exist! Please add an Android platform.');
+}
+
+dest = path.join(dest, 'AndroidManifest.xml');
+
+// fs.createReadStream(manifestPath).pipe(fs.createWriteStream(dest));
+
+var rd = fs.createReadStream(manifestPath);
+rd.on("error", function(err) {
+    quit(err);
+});
+var wr = fs.createWriteStream(dest);
+wr.on("error", function(err) {
+    quit(err);
+});
+rd.pipe(wr);
